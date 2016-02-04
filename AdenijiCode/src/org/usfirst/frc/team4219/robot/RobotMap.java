b3447fc67b76b9af4e8d3a779088be6a5322437c
ROBOTICS <r4satx@gmail.com>
2016-02-04 16:47:08 -0600

    Jason Conklin - Robotics Club President 2/4/16
    First upload of our FRC code using git as far as I know. I am uploading
    all of previous president Adeniji Ogunlana's code used for last year's
    FRC T-Shirt Cannon Robot.

---
+++ AdenijiCode/src/org/usfirst/frc/team4219/robot/RobotMap.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team4219.robot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import org.usfirst.frc.team4219.robot.subsystems.ExampleSubsystem;
+import org.usfirst.frc.team4219.robot.commands.ExampleCommand;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+    // For example to map the left and right motors, you could define the
+    // following variables to use with your drivetrain subsystem.
+    // public static int leftMotor = 1;
+    // public static int rightMotor = 2;
+    
+    // If you are using multiple modules, make sure to define both the port
+    // number and the module. For example you with a rangefinder:
+    // public static int rangefinderPort = 1;
+    // public static int rangefinderModule = 1;
+}
